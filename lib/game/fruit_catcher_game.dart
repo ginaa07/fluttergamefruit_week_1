f2c53f2014d4a50556ca5f49d52d9d7cde82189c
ginaa07 <1123150124@global.ac.id>
2026-02-15 16:09:31 +0700

    penambahan class and backgroundcolor, notifierscore di fruit chatcer game

---
+++ lib/game/fruit_catcher_game.dart
@@ -0,0 +1,16 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FruitCatcherGame extends FlameGame {
+
+
+  @override
+  Color backgroundColor() => const Color(0xFF87CEEB); // sky blue
+ 
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
+}
