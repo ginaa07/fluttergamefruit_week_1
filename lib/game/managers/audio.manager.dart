13c8fe806bfaf0a8a5898ea10da6c4ba3d22c63d
ginaa07 <1123150124@global.ac.id>
2026-02-16 20:16:31 +0700

    create void togglemusic

--- lib/game/managers/audio.manager.dart
+++ lib/game/managers/audio.manager.dart
@@ -111,3 +111,12 @@ print('Error setting music volume: $e');
 void setSfxVolume(double volume) {
 _sfxVolume = volume.clamp(0.0, 1.0);
 }
+
+void toggleMusic() {
+_isMusicEnabled = !_isMusicEnabled;
+if (_isMusicEnabled) {
+resumeBackgroundMusic();
+} else {
+pauseBackgroundMusic();
+}
+}
