bea7ac05491f76eb47fa0c0e1a149178ed1d1a97
ginaa07 <1123150124@global.ac.id>
2026-02-16 19:45:10 +0700

    create import for file fruit

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -1,1 +1,7 @@
-fruit
+import 'dart:math';
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import 'package:latihanfluttergame_week1/game/fruit_catcher_game.dart';
+import 'basket.dart';
