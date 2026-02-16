1c4448b7e89ce2970330848d0f62b2766f00515a
ginaa07 <1123150124@global.ac.id>
2026-02-16 20:14:51 +0700

    penambahan final adjustedvolume

--- lib/game/managers/audio.manager.dart
+++ lib/game/managers/audio.manager.dart
@@ -88,5 +88,11 @@ print('Error playing SFX: $e');
 void playSfxWithVolume(String fileName, double volume) {
 if (_isSfxEnabled) {
 try {
-final adjustedVolume = (volume * _sfxVolume).clamp(0.
-0, 1.0);
+final adjustedVolume = (volume * _sfxVolume).clamp(0.0, 1.0);
+FlameAudio.play('sfx/$fileName', volume: adjustedVolu
+me);
+} catch (e) {
+print('Error playing SFX with volume: $e');
+}
+}
+}
