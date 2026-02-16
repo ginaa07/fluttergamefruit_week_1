dd37a58d2d104b5e8ac2a0df74133fa024e46f30
ginaa07 <1123150124@global.ac.id>
2026-02-16 20:06:17 +0700

    penambahan getters bool get ismuisc, sfx and double get music and sfxvolume in file audio manager

--- lib/game/managers/audio.manager.dart
+++ lib/game/managers/audio.manager.dart
@@ -8,3 +8,9 @@ bool _isMusicEnabled = true;
 bool _isSfxEnabled = true;
 double _musicVolume = 0.7;
 double _sfxVolume = 1.0;
+
+// Getters
+bool get isMusicEnabled => _isMusicEnabled;
+bool get isSfxEnabled => _isSfxEnabled;
+double get musicVolume => _musicVolume;
+double get sfxVolume => _sfxVolume;
