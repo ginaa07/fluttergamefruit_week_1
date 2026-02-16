50019fa99dabe48466a10ffd7137ba518df527ff
ginaa07 <1123150124@global.ac.id>
2026-02-16 20:11:27 +0700

    create void for stop background music flameaudio

--- lib/game/managers/audio.manager.dart
+++ lib/game/managers/audio.manager.dart
@@ -43,3 +43,12 @@ print('Error playing background music: $e');
 }
 }
 }
+
+//Stop background music
+void stopBackgroundMusic() {
+try {
+FlameAudio.bgm.stop();
+} catch (e) {
+print('Error stopping background music: $e');
+}
+}
