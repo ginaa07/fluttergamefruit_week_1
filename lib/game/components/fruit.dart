2103ad603df3d2c77c3d74998c57654e99d48eb2
ginaa07 <1123150124@global.ac.id>
2026-02-16 19:47:45 +0700

    create class fruit

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -5,3 +5,11 @@ import 'package:flutter/material.dart';
 
 import 'package:latihanfluttergame_week1/game/fruit_catcher_game.dart';
 import 'basket.dart';
+
+  enum FruitType { apple, banana, orange, strawberry }
+
+  class Fruit extends PositionComponent 
+    with HasGameRef<FruitCatcherGame>, CollisionCallbacks {
+      final FruitType type;
+      final double fallSpeed = 200;
+      
