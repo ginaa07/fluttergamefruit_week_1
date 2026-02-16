b3c629fadb5ecebe390560fe740312e74939d08b
ginaa07 <1123150124@global.ac.id>
2026-02-16 22:47:00 +0700

    penambahan final rect fi file basket

--- lib/game/components/basket.dart
+++ lib/game/components/basket.dart
@@ -2,8 +2,7 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class Basket extends PositionComponent
-    with HasGameRef, CollisionCallbacks {
+class Basket extends PositionComponent with HasGameRef, CollisionCallbacks {
   Basket() : super(size: Vector2(80, 60));
 
   @override
@@ -14,25 +13,30 @@ class Basket extends PositionComponent
     anchor = Anchor.center;
     add(RectangleHitbox());
   }
-   
-   @override
-   void render(Canvas canvas) {
+
+  @override
+  void render(Canvas canvas) {
     super.render(canvas);
 
     final paint = Paint()
-    ..color = Colors.brown
-    ..style = PaintingStyle.fill;
+      ..color = Colors.brown
+      ..style = PaintingStyle.fill;
 
     //Draw basket
+    final rect = RRect.fromRectAndRadius(
+      Rect.fromLTWH(0, 0, size.x, size.y),
+      const Radius.circular(10),
+    );
+
     final handlePaint = Paint()
-    ..color = Colors.brown[800]!
-    ..style = PaintingStyle.stroke
-    ..strokeWidth = 3;
+      ..color = Colors.brown[800]!
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3;
 
     final handlePath = Path()
-    ..moveTo(10, 0)
-    ..quadraticBezierTo(size.x / 2, -20, size.x - 10, 0);
-    
+      ..moveTo(10, 0)
+      ..quadraticBezierTo(size.x / 2, -20, size.x - 10, 0);
+
     canvas.drawPath(handlePath, handlePaint);
-   }
+  }
 }
