95aef553c4adc0fc0fe7ff67dbc671a20e004f70
ginaa07 <1123150124@global.ac.id>
2026-02-16 16:23:36 +0700

    update class fruitcatchergame with extends and late basket, final random

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -5,7 +5,14 @@ import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-class FruitCatcherGame extends FlameGame {
+
+
+class FruitCatcherGame extends FlameGame with PanDetector, HasCollisionDetection {
+  late Basket basket;
+  late TextComponent scoreText;
+  final Random random = Random();
+  double fruitSpawnTimer = 0;
+  final double fruitSpawnInterval = 1.5;
 
   @override
   Color backgroundColor() => const Color(0xFF87CEEB); // sky blue
